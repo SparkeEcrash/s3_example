9bff7f07e9ddd7b8f2cbc26bd824535d6f7b4983
Scott Bowler <scott.bowler@learningfuze.com>
2019-01-09 21:46:48 -0800

    Initial setup complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Amazon s3 Example
+
+> Example project for uploading an image to an Amazon s3 server utilizing ReactJS and NodeJS
---
+++ client/webpack.common.js
@@ -0,0 +1,41 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    context: resolve(__dirname, 'src'),
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?(c|a)ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: ['file-loader?hash=sha512&digest=hex&name=assets/images/[hash].[ext]']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'assets/fonts/'
+                    }
+                }
+            }
+        ]
+    }
+};
