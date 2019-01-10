1a29cb067cb83559f819d5e01b1a9ef6d8ea5345
Scott Bowler <scott.bowler@learningfuze.com>
2019-01-10 01:40:07 -0800

    Initial file to s3 with path saved in MySQL DB working

--- client/src/reducers/images_reducer.js
+++ client/src/reducers/images_reducer.js
@@ -1,11 +1,13 @@
 import types from '../actions/types';
 
 const DEFAULT_STATE = {
-
+    all: []
 };
 
 export default (state = DEFAULT_STATE, action) => {
     switch(action.type){
+        case types.GET_IMAGES:
+            return { ...state, all: action.images };
         default:
             return state;
     }
